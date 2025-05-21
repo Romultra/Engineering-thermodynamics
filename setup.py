18d641cd32143adeacbfda3f47e7ff1b99b8bd8c
Romultra <romultra@gmail.com>
2025-05-21 16:22:13 +0200

    Add setup.py for packaging and distribution

---
+++ setup.py
@@ -0,0 +1,12 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name='thermo',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[],  # list dependencies here
+    description='Tiny Thermodynamics Library',
+    author='Romultra',
+    author_email='romultra@gmail.com',
+)
